5e6d51c838b051b148f524e85705d8e1c9796c3c
Yen <ny3062001@gmail.com>
2023-04-02 23:21:47 +0700

    sua file install

--- installmanager.sh
+++ installmanager.sh
@@ -1,28 +1,45 @@
 #!/bin/sh 
 
-command=$1 
+ command=$1 
 
-if [ -z $command ] 
-then 
-    echo 'Chua nhap chuc nang' 
-fi 
+ if [ -z $command ] 
 
-if [ $command = "install" ] 
-then 
-    if [ `whoami` != "root" ] 
-    then 
-        echo 'Ban can phai chay lenh sudo de thay doi he thong' 
-        exit $E_NOTROOT 
-    fi 
-    cat input.txt | xargs sudo apt-get -y install 
-fi 
+     then echo 'Chua nhap chuc nang' 
 
-if [ $command = "uninstall" ] 
-then 
-    if [ `whoami` != "root" ] 
-    then 
-        echo 'Ban can phai chay lenh sudo de thay doi he thong' 
-        exit $E_NOTROOT 
-    fi 
-    cat input.txt | xargs sudo apt-get -y purge 
-fi 
+ fi 
+
+ if [ $command = "install" ] 
+
+     then 
+
+         if [ `whoami` != "root" ] 
+
+             then 
+
+             echo 'Ban can phai chay lenh sudo de thay doi he thong' 
+
+             exit $E_NOTROOT 
+
+         fi 
+
+         cat input.txt | xargs sudo apt-get -y install 
+
+ fi 
+
+ if [ $command = "uninstall" ] 
+
+     then 
+
+         if [ `whoami` != "root" ] 
+
+             then 
+
+             echo 'Ban can phai chay lenh sudo de thay doi he thong' 
+
+             exit $E_NOTROOT 
+
+         fi 
+
+         cat input.txt | xargs sudo apt-get -y purge 
+
+ fi 
